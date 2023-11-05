ea189f12c04254aba178953208a10d10c844f6fd
Suraj Patra <surajpatra2908@gmail.com>
2023-11-05 12:04:19 +0530

    sbsbjs

--- src/Validate/Login.jsx
+++ src/Validate/Login.jsx
@@ -12,7 +12,7 @@ function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     // const response = await fetch("https://nice-lime-giraffe-coat.cyclic.app/api/loginuser", {
-      const response = await fetch("http://localhost:3500/api/loginuser", {
+      const response = await fetch("https://proud-pike-fez.cyclic.app/api/loginuser", {
       // mode: 'no-cors',
       // cache: 'no-cache',
 
